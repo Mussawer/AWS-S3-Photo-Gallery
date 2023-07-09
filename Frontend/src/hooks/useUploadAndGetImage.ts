29c191676d6c48adb4f694e2f25e18c120a2ba31
Mussawer.Raza-at-101380220521 <mussawer@gosaas.io>
2023-07-09 05:15:49 +0500

    invalidate query

--- Frontend/src/hooks/useUploadAndGetImage.ts
+++ Frontend/src/hooks/useUploadAndGetImage.ts
@@ -1,20 +1,28 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getImageFromS3, uploadImageToS3 } from "../services/UploadAndGetImageFromS3";
-
+import {
+  getImageFromS3,
+  uploadImageToS3,
+} from "../services/UploadAndGetImageFromS3";
 
 export const useGetImage = () => {
-  const {isLoading, data} = useQuery(['image-from-s3'],  () => getImageFromS3());
-  console.log("🚀 ~ file: useUploadAndGetImage.ts:7 ~ useGetImage ~ isLoading:", isLoading)
-  return {isLoading, data}
+  const { isLoading, data, refetch, status } = useQuery(
+    ["image-from-s3"],
+    () => getImageFromS3(),
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+  console.log(
+    "🚀 ~ file: useUploadAndGetImage.ts:7 ~ useGetImage ~ isLoading:",
+    isLoading
+  );
+  return { isLoading, data, refetch };
 };
 
 export const useUploadImage = () => {
-  const queryClient = useQueryClient()
-  return useMutation((args: [FormData]) => uploadImageToS3(...args),{
-    onSuccess: () => {
-      queryClient.invalidateQueries(['image-from-s3'])
-    }
+  const queryClient = useQueryClient();
+  return useMutation((args: [FormData]) => uploadImageToS3(...args), {
+    onSuccess: () =>
+      queryClient.invalidateQueries(["image-from-s3"]),
   });
 };
-
-
--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} position='top-right'/>
   </QueryClientProvider>,
   document.getElementById("root")
 );
